53a61846d5e13423aa52e92a1611254ceb7c8c09
pawel.niechoda <pawel.niechoda@avaus.fi>
2015-03-01 21:36:30 +0200

    Implement centering on selected store

--- www/js/controllers/store-list.js
+++ www/js/controllers/store-list.js
@@ -1,8 +1,15 @@
 'use strict';
 
-controllers.controller('StoreListController', function($scope, Stores) {
+controllers.controller('StoreListController', function($scope, Stores, Map) {
     var self = this;
     Stores.getStores().then(function(stores) {
         self.stores = stores;
     });
+
+    this.centerOn = function(store) {
+        if (store && store.lat && store.lng) {
+            Map.centerOn(store.lat, store.lng);
+        }
+    }
+
 });
