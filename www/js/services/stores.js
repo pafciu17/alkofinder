22b2899e4fd0ca1851b41f42fc7b38edffa7b3b6
pawel.niechoda <pawel.niechoda@avaus.fi>
2015-03-01 21:12:15 +0200

    Remove test data from Stores service, refactor service to cache store data. Add storing of fetching promise to prevent multiple requests.

--- www/js/services/stores.js
+++ www/js/services/stores.js
@@ -1,20 +1,36 @@
 'use strict';
 
 services.service('Stores', function($q, $http) {
-    var stores = [
-        { title: 'Reggae', id: 1, lat: 37.779, lon: -122.3892},
-        { title: 'Reggae', id: 1, lat: 37.759, lon: -122.3842},
-        { title: 'Reggae', id: 1, lat: 37.739, lon: -122.2422},
-        { title: 'Reggae', id: 1, lat: 37.839, lon: -122.2022},
-        { title: 'Reggae', id: 1, lat: 37.939, lon: -122.1822},
-    ];
+
+    var stores = [],
+        urlToStoreBackend = 'http://pnie.eu:3001',
+        fetchStorePromise;
+
+    var sort = function(data) {
+        return _.sortBy(data, function(item) {
+            return item.Name;
+        });
+    };
+
+    var fetchStores = function() {
+        if (!fetchStorePromise) {
+            fetchStorePromise = $http.get(urlToStoreBackend).then(function(response) {
+                if (response && response.data) {
+                    stores = sort(response.data)
+                }
+                fetchStorePromise = null;
+                return stores;
+            });
+        }
+        return fetchStorePromise;
+    };
 
     this.getStores = function() {
-        var deferred = $q.defer();
-        $http.get('http://pnie.eu:3001').then(function(response) {
-            console.log(response.data[0]);
-            deferred.resolve(response.data);
-        });
-        return deferred.promise;
+        if (stores.length === 0) {
+            return fetchStores();
+        }
+        return $q.when(stores);
     };
+
+
 });
